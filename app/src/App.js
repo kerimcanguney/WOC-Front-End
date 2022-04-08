130dcf743c24a5c458c9453fca44d071c8d93fee
Kerimcan <464639@student.fontys.nl>
2022-04-08 11:58:40 +0200

    Removed steps + added login w fetch req

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,40 @@
-import React from 'react';
-import CreationPage from './components/creationpage/index.js'
+import React, {useState} from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Dashboard from './components/Dashboard';
+import Login from './components/LoginForm/login';
+import Preferences from './components/Prefrences';
+import useToken from './useToken';
+
+// function setToken(userToken) {
+//   sessionStorage.setItem('token', JSON.stringify(userToken));
+// }
+
+// function getToken() {
+//   const tokenString = sessionStorage.getItem('token');
+//   const userToken = JSON.parse(tokenString);
+//   return userToken?.token
+// }
+
+
 function App() {
+  const {token, setToken}= useToken();
+  if (!token){
+    return <Login setToken={setToken}/>
+  }
   return (
-    <div className="App">
-        <CreationPage/>
-    </div>
+    <>
+    <h1>Applicatie</h1>
+      <BrowserRouter>
+        <Routes>
+        <Route path='/preferences' element={<Preferences/>} />
+        <Route path='/dashboard' element={<Dashboard/>} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
